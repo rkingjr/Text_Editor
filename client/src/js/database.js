74bf39a73cf05e03f362cf6ed7b38a4d470c5a9b
Randy King Jr <rkingjr@outlook.com>
2022-03-23 13:17:18 -0500

    update put method in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,17 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("PUT request to update jateDB");
+  const jateDB=await openDB("jate",1);
+  const tx=jateDB.transaction("jate","readwrite");
+  const objStore=tx.objectStore("jate");
+  const req=objStore.put({content:content})
+  const res=await req;
+  console.log("Data saved to jateDB.", res);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async (value) => console.error('getDb not implemented');
 
 initdb();
